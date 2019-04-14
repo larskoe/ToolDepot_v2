dbcdbe1abb6cbb2424d67030869c794389096c0a
Lars Koester <lkoester2015@fau.edu>
2019-04-14 18:07:27 -0400

    added JavaDoc and comments to Product Page

--- src/productPage/MainController.java
+++ src/productPage/MainController.java
@@ -18,27 +18,32 @@ import loginRegister.LoginView;
  */
 public class MainController {
 
-	
+	/**
+	 * Logout function with an alert box to confirm logout
+	 * @param event event handler
+	 * @throws Exception throws an exception if it detects an error
+	 */
 	void logout (ActionEvent event) throws Exception {
 		
+		//sets up alert box
 		Alert alert = new Alert(AlertType.WARNING);
 		alert.setTitle("Alert Logout");
 		alert.setHeaderText("Are you sure you want to logout?");
 		alert.setContentText("Are you sure?");
 		
+		//sets up yes and no button
 		ButtonType buttonTypeNo = new ButtonType("No");
 		ButtonType buttonTypeYes = new ButtonType("Yes");
-		
 		alert.getButtonTypes().setAll(buttonTypeYes, buttonTypeNo);
-
 		Optional<ButtonType> result = alert.showAndWait();
+		
+		//if button is yes, log out, else do nothing
 		if (result.get() == buttonTypeYes){
 			Platform.exit();
 			System.exit(0);
 		} else {
 		    //do nothing
 		}
-		
 	}
 
 }
